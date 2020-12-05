a8635445c8c7fe00126c896d040155ffcbda3977
Jiang <505514870@qq.com>
2020-12-05 16:02:10 +0800

    compilers: simple language add if

--- compilers/ruby/simple.rb
+++ compilers/ruby/simple.rb
@@ -107,7 +107,7 @@ class Machine < Struct.new(:statement, :environment)
 
   def run
     while statement.reducible?
-      puts "#{statement}, #{environment}, #{aaa}"
+      puts "#{statement}, #{environment}"
       step
     end
     puts "#{statement}, #{environment}"
@@ -174,6 +174,58 @@ class Assign < Struct.new(:name, :expression)
 
 end
 
+class If < Struct.new(:condition, :consequence, :alternative) 
+  def to_s
+    "if (#{condition}) { #{consequence} } else { #{alternative} }"
+  end
+
+  def inspect
+    "《#{self}》"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if condition.reducible?
+      [If.new(condition.reduce(environment), consequence, alternative), environment]
+    else
+      case condition
+      when Boolean.new(true)
+        [consequence, environment]
+      when Boolean.new(false)
+        [alternative, environment]
+      end
+    end
+  end
+
+end
+
+class Sequence < Struct.new(:first, :second)
+  def to_s
+    "#{first}; #{second}"
+  end
+
+  def inspect
+    "《#{self}》"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    case first
+    when DoNothing.new
+      [second, environment]
+    else
+      reduced_first, reduced_environment = first.reduce(environment)
+      [Sequence.new(reduced_first, second), reduced_environment]
+    end
+  end
+
+end
 
 # Machine.new(
 #   Add.new(Variable.new(:x), Variable.new(:y)),
@@ -181,7 +233,29 @@ end
 # ).run
 
 
+# Machine.new(
+#   Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
+#   { x: Number.new(2) }
+# ).run
+
 Machine.new(
-  Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
-  { x: Number.new(2) }
+  If.new(
+    Variable.new(:x),
+    Assign.new(:y, Number.new(1)),
+    Assign.new(:y, Number.new(2))
+  ),
+  { x: Boolean.new(true) }
+).run
+
+Machine.new(
+  If.new(Variable.new(:x), Assign.new(:y, Number.new(1)), DoNothing.new),
+  { x: Boolean.new(false) }
+).run
+
+Machine.new(
+  Sequence.new(
+    Assign.new(:x, Add.new(Number.new(1), Number.new(2))),
+    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
+  ),
+  {}
 ).run
