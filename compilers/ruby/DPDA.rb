79a7a0df1b67732303da5f54d625158b0a8e6fbd
Jiang <505514870@qq.com>
2021-01-07 22:54:30 +0800

    feat(compilers): use DPDA mock palindrome

--- compilers/ruby/DPDA.rb
+++ compilers/ruby/DPDA.rb
@@ -17,6 +17,14 @@ class Stack <  Struct.new(:contents)
 end
 
 class PDAConfiguration < Struct.new(:state, :stack)
+  STUCK_STATE = Object.new
+  def stuck 
+    PDAConfiguration.new(STUCK_STATE, stack)
+  end
+
+  def stuck?
+    state == STUCK_STATE
+  end
 end
 
 class PDARule < Struct.new(:state, :character, :next_state, :pop_character, :push_characters)
@@ -65,13 +73,25 @@ class DPDA < Struct.new(:current_configuration, :accept_states, :rulebook)
   end
 
   def read_character(character)
-    self.current_configuration = rulebook.next_configuration(current_configuration, character)
+    self.current_configuration = (next_configuration(character))
   end
 
   def read_string(string)
     string.chars.each do |character|
-      read_character(character)
+      read_character(character) unless stuck?
     end
+  end
+
+  def next_configuration(character) 
+    if rulebook.applies_to?(current_configuration, character)
+      rulebook.next_configuration(current_configuration, character)
+    else
+      current_configuration.stuck
+    end
+  end
+
+  def stuck?
+    current_configuration.stuck?
   end
 
   def current_configuration
@@ -125,3 +145,41 @@ dpda_design = DPDADesign.new(1, '$', [1], rulebook)
 dpda_design.accepts?('(((((((((())))))))))')
 
 dpda_design.accepts?('(()(()(()()(()()))()')
+
+rulebook2 = DPDARulebook.new([
+  PDARule.new(1, 'a', 2, '$', ['a', '$']),
+  PDARule.new(1, 'b', 2, '$', ['b', '$']),
+  PDARule.new(2, 'a', 2, 'a', ['a', 'a']),
+  PDARule.new(2, 'b', 2, 'b', ['b', 'b']),
+  PDARule.new(2, 'a', 2, 'b', []),
+  PDARule.new(2, 'b', 2, 'a', []),
+  PDARule.new(2, nil, 1, '$', ['$']),
+])
+
+dpda_design = DPDADesign.new(1, '$', [1], rulebook2)
+
+dpda_design.accepts?('ababab')
+
+dpda_design.accepts?('bbbaaaab')
+
+dpda_design.accepts?('baa')
+
+
+rulebook3 = DPDARulebook.new([
+  PDARule.new(1, 'a', 1, '$', ['a', '$']),
+  PDARule.new(1, 'a', 1, 'a', ['a', 'a']),
+  PDARule.new(1, 'a', 1, 'b', ['a', 'b']),
+  PDARule.new(1, 'b', 1, '$', ['b', '$']),
+  PDARule.new(1, 'b', 1, 'a', ['b', 'a']),
+  PDARule.new(1, 'b', 1, 'b', ['b', 'b']),
+  PDARule.new(1, 'm', 2, '$', ['$']),
+  PDARule.new(1, 'm', 2, 'a', ['a']),
+  PDARule.new(1, 'm', 2, 'b', ['b']),
+  PDARule.new(2, 'a', 2, 'a', []),
+  PDARule.new(2, 'b', 2, 'b', []),
+  PDARule.new(2, nil, 3, '$', ['$']),
+])
+
+dpda_design = DPDADesign.new(1, '$', [3], rulebook3)
+dpda_design.accepts?('abmba')
+dpda_design.accepts?('abma')
