a281c5485c4c1139177942a70654e510e96252e9
Jiang <505514870@qq.com>
2021-01-21 21:25:13 +0800

    feat(compilers): proc list

--- compilers/ruby/proc.rb
+++ compilers/ruby/proc.rb
@@ -3,6 +3,9 @@ ZERO = -> p { -> x { x } }
 ONE = -> p { -> x { p[x] } }
 TWO = -> p { -> x { p[p[x]] } }
 THREE = -> p { -> x { p[p[p[x]]] } }
+FIVE = -> p { -> x { p[p[p[p[p[x]]]]] } }
+FIFTEEN = -> p { -> x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[x]]]]]]]]]]]]]]] } }
+HUNDRED = -> p { -> x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[ p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[ p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[x]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] } }
 
 def to_integer(proc) 
   proc[-> n { n + 1 }][0]
@@ -151,3 +154,109 @@ end
 to_array(my_list)
 
 to_array(my_list).map { |p| to_integer(p) }
+
+RANGE = 
+  Z[ -> f {
+    -> m { -> n {
+      IF[IS_LESS_OR_EQUAL[m][n]][
+        -> x {
+          UNSHIFT[f[INCREMENT[m]][n]][m][x]
+        }
+      ][
+        EMPTY
+      ]
+    }}
+  }]
+
+my_range = RANGE[ONE][FIVE]
+
+to_array(my_range).map { |p| to_integer(p) }
+
+FOLD = 
+  Z[ -> f {
+    -> l { -> x { -> g {
+      IF[IS_EMPTY[l]][
+        x
+      ][
+        -> y {
+          g[f[REST[l]][x][g]][FIRST[l]][y]
+        }
+      ]
+    }}}
+  }]
+
+MAP = 
+  -> k { -> f {
+    FOLD[k][EMPTY][
+      -> l { -> x { UNSHIFT[l][f[x]] } }
+    ]
+  }}
+
+  my_list = MAP[RANGE[ONE][FIVE]][INCREMENT]
+  to_array(my_list).map { |p| to_integer(p) }
+
+TEN = MULTIPLY[TWO][FIVE]
+B = TEN 
+F = INCREMENT[B] 
+I = INCREMENT[F] 
+U = INCREMENT[I] 
+ZED = INCREMENT[U]
+FIZZ = UNSHIFT[UNSHIFT[UNSHIFT[UNSHIFT[EMPTY][ZED]][ZED]][I]][F] 
+BUZZ = UNSHIFT[UNSHIFT[UNSHIFT[UNSHIFT[EMPTY][ZED]][ZED]][U]][B] 
+FIZZBUZZ = UNSHIFT[UNSHIFT[UNSHIFT[UNSHIFT[BUZZ][ZED]][ZED]][I]][F]
+
+def to_char(c) 
+  '0123456789BFiuz'.slice(to_integer(c))
+end
+
+def to_string(s) 
+  to_array(s).map { |c| to_char(c) }.join
+end
+
+to_char(ZED)
+
+DIV = 
+  Z[-> f { -> m { -> n {
+    IF[IS_LESS_OR_EQUAL[n][m]][
+      -> x {
+        INCREMENT[f[SUBTRACT[m][n]][n]][x]
+      }
+    ][
+      ZERO
+    ]
+  }}}]
+PUSH =
+  -> l {
+    -> x {
+      FOLD[l][UNSHIFT[EMPTY][x]][UNSHIFT]
+    }
+  }
+
+TO_DIGITS =
+  Z[-> f { -> n { 
+    PUSH[ IF[IS_LESS_OR_EQUAL[n][DECREMENT[TEN]]
+    ][ EMPTY ][ 
+      -> x { 
+        f[DIV[n][TEN]][x] 
+      } 
+    ] 
+  ][MOD[n][TEN]] } }]
+
+to_array(TO_DIGITS[FIVE]).map { |p| to_integer(p) }
+
+to_string(TO_DIGITS[FIVE])
+to_string(TO_DIGITS[POWER[FIVE][THREE]])
+
+solution = 
+  MAP[RANGE[ONE][HUNDRED]][-> n { 
+    IF[IS_ZERO[MOD[n][FIFTEEN]]][ 
+      FIZZBUZZ 
+    ][IF[IS_ZERO[MOD[n][THREE]]][ 
+      FIZZ 
+    ][IF[IS_ZERO[MOD[n][FIVE]]][ 
+      BUZZ 
+    ][ 
+      TO_DIGITS[n] 
+    ]]] 
+  }]
+  to_array(solution).each do |p| puts to_string(p) end; nil
